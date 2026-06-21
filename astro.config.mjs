358100b0018c02610a7d999f1244caad824d98cf
Pablo Díaz <pablomigueldiazvizcaino@gmail.com>
2026-06-21 16:03:33 -0400

    update github

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/invitaciones-boda-anime/',
   vite: {
     plugins: [tailwindcss()]
   }
